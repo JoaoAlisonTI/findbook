d024962a7f837c47265ed0566260f2653b867d50
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-06-05 11:42:56 -0300

    feat: adicionando o component Container

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import backgroundImage from '../../assets/images/background-header.png';
 import { HeaderTitle } from '../HeaderTitle/HeaderTitle'
+import { Container } from '../Container/Container'
 
 export function Header() {
   return (
@@ -9,13 +10,15 @@ export function Header() {
       }}
       className=" w-auto bg-cover bg-center bg-no-repeat"
     >
-      <HeaderTitle />
-      <div className="mt-28">
-        <p className="md:text-7xl text-3xl font-bold text-evergreen">
-            Encontre livros <br /> que seja a sua cara!
-        </p>
-        <p className="text-gray-500 my-5 text-xl">Lorem ipsum</p>
-      </div>
+      <Container>
+        <HeaderTitle />
+        <div className="mt-28">
+          <p className="md:text-7xl text-3xl font-bold text-evergreen">
+              Encontre livros <br /> que seja a sua cara!
+          </p>
+          <p className="text-gray-500 my-5 text-xl">Lorem ipsum</p>
+        </div>
+      </Container>
     </header>
   )
 }
